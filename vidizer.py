331ebd7dd71df9d4cae8cb968298bbcef96a99d4
HassanAbbas7 <ph015016test1@gmail.com>
2023-07-25 21:20:32 +0500

    first final version

--- vidizer.py
+++ vidizer.py
@@ -4,8 +4,8 @@ from makeVideo import vidize
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="My script description")
-    parser.add_argument("--vidize", type=str, help="Description of argument 1")
-    parser.add_argument("--filify", type=bool, default=False, help="Description of argument 2")
+    parser.add_argument("--vidize", type=str, help="Give file name like --vidize sample.rar")
+    parser.add_argument("--filify", type=bool, default=False, help="set true for converting a video like --filify true")
 
     args = parser.parse_args()
     if (args.vidize):
